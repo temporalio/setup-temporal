cfad12a7c50962a28153ba96dfac59129ba526b9
James Watkins-Harvey <james.watkinsharvey@temporal.io>
2023-04-07 11:39:56 -0700

    'fetch' wasn't imported

--- src/download-info.ts
+++ src/download-info.ts
@@ -1,13 +1,15 @@
-export interface Info {
+import { got } from 'got';
+
+export interface DownloadInfo {
   archiveUrl: string;
   fileToExtract: string;
 }
 
-export async function getDownloadInfo(version: string, platform: string, arch: string): Promise<Info> {
-  const infoUrl = `https://temporal.download/cli/${version}?platform=${platform}&arch=${arch}`;
-  const infoRes = await fetch(infoUrl);
-
-  if (infoRes.ok) return (await infoRes.json()) as Info;
-
-  throw new Error(`Failed to get download info: ${infoRes.statusText}`);
+export async function getDownloadInfo(version: string, platform: string, arch: string): Promise<DownloadInfo> {
+  try {
+    const infoUrl = `https://temporal.download/cli/${version}?platform=${platform}&arch=${arch}`;
+    return await got(infoUrl).json<DownloadInfo>();
+  } catch (e) {
+    throw new Error(`Failed to get download info: ${e}`);
+  }
 }
